d6fc450bcef989d2fcf1085a307128282d65e3eb
steve-anunknown <steve.anunknown@gmail.com>
2023-08-10 21:37:57 +0300

    i fixed karagkiozilikia

--- codegen.ml
+++ codegen.ml
@@ -1,7 +1,6 @@
 open Ast
 open Llvm
 
-
 type llvm_info = {
   context          : Llvm.llcontext;
   the_module       : Llvm.llmodule;
@@ -17,74 +16,59 @@ type llvm_info = {
   the_writeString  : Llvm.llvalue;
 }
 
-let codegen_expr info ast =
+let rec codegen_expr info ast =
   match ast with
-  | EInt        i    -> codegen_int       info i
-  | EChar       c    -> codegen_char      info c
-  | EUnOp       op   -> codegen_uop       info op
-  | EBinOp      op   -> codegen_binop     info op
-  | ELVal       lval -> codegen_lval      info lval (* not implemented yet*)
-  | EFuncCall   fn   -> codegen_func_call info fn   (* not implemented yet*)
+  | EInt(i, _)                    -> codegen_int       info i
+  | EChar(c, _)                   -> codegen_char      info c
+  | EUnOp(op, expr, _)            -> codegen_uop       info op (codegen_expr info expr)
+  | EBinOp(op, expr1, expr2, _)   -> codegen_bop       info op (codegen_expr info expr1) (codegen_expr info expr2)
+  | ELVal(lval, _)                -> codegen_lval      info lval (* not implemented yet*)
+  | EFuncCall(fn, expr_list, _)   -> codegen_func_call info fn (List.iter (codegen_expr info) expr_list)  (* not implemented yet*)
 
-let codegen_int  info i = info.c64 i
-let codegen_char info c = info.c32 (Char.code c)
-let codegen_uop info oper =
+and codegen_int  info i = info.c64 i
+and codegen_char info c = info.c32 (Char.code c)
+and codegen_uop info oper expr =
   match oper with
-  | EUnOp(op, expr, _) -> let e = codegen_expr info expr in
-                            begin
-                              match op with
-                              | UnopPlus  -> e
-                              | UnopMinus -> Llvm.build_neg e "negtmp" info.builder
-                            end
+  | UnopPlus -> expr
+  | UnopNeg  -> Llvm.build_neg expr "negtmp" info.builder
 
-let codegen_bop info oper =
+and codegen_bop info oper expr1 expr2 =
   match oper with
-  | EBinOp(op, expr1, expr2) -> let e1 = codegen_expr info expr1
-                                and e2 = codegen_expr info expr2 in
-                                  begin
-                                    match op with
-                                    | BopAdd -> Llvm.build_add e1 e2 "addtmp" info.builder
-                                    | BopSub -> Llvm.build_sub e1 e2 "subtmp" info.builder
-                                    | BopMul -> Llvm.build_mul e1 e2 "multmp" info.builder
-                                    | BopDiv -> Llvm.build_sdiv e1 e2 "divtmp" info.builder
-                                    | BopMod -> Llvm.build_srem e1 e2 "modtmp" info.builder
-                                  end
+  | BopAdd -> Llvm.build_add expr1 expr2 "addtmp" info.builder
+  | BopSub -> Llvm.build_sub expr1 expr2 "subtmp" info.builder
+  | BopMul -> Llvm.build_mul expr1 expr2 "multmp" info.builder
+  | BopDiv -> Llvm.build_sdiv expr1 expr2 "divtmp" info.builder
+  | BopMod -> Llvm.build_srem expr1 expr2 "modtmp" info.builder
 
-let codegen_lval info lval = raise "Not implemented"
-let codegen_func_call info fn = raise "Not implemented"
-
-let codegen_stmt info statement =
+let rec codegen_stmt info statement =
   match statement with
-  | EEmpty                         -> ((*do nothing*))
-  | EBlock(stmts)                  -> codegen_block     info stmts
-  | ECallFunc(fn, params)          -> codegen_func_call info fn params  (* not implemented yet *)
+  | EEmpty(_)                      -> ((*do nothing*))
+  | EBlock(stmts, _)               -> codegen_block     info stmts
+  | ECallFunc(fn, params, _)       -> codegen_func_call info fn params  (* not implemented yet *)
   | EAss(lval, expr, _)            -> codegen_ass       info lval expr
   | EIf(cond, stmt, _)             -> codegen_if        info cond stmt
   | EIfElse(cond, stmt1, stmt2, _) -> codegen_ifelse    info cond stmt1 stmt2
   | EWhile(cond, stmt, _)          -> codegen_while     info cond stmt
-  | ERet                           -> codegen_ret       info
+  | ERet(_)                        -> codegen_ret       info
   | ERetVal(expr, _)               -> codegen_retval    info expr
 
-let codegen_block info stmt_list = 
-  List.iter (codegen_stmt info) stmt_list
+and codegen_block info block =
+  match block with
+  | EListStmt(stmt_list, _) -> List.iter (codegen_stmt info) stmt_list
 
-let codegen_func_call info fn params = raise "Not implemented"
-
-let codegen_ass info lval expr = raise "Not implemented"
-
-let rec codegen_cond info cond =
+and codegen_cond info cond =
   match cond with
-  | ELbop(lbop, cond1, cond2, _)  ->  let c1 = codegen_cond info cond1
-                                      and c2 = codegen_cond info cond2 in
-                                      match lbop with
+  | ELbop(op, cond1, cond2, _)  ->  let c1 = codegen_cond info cond1
+                                    and c2 = codegen_cond info cond2 in
+                                      match op with
                                       | LbopAnd -> Llvm.build_and c1 c2 "andtemp" info.builder
                                       | LbopOr  -> Llvm.build_or  c1 c2 "ortemp"  info.builder
-  | ELuop(luop, cond, _)          ->  let c = codegen_cond info cond in
-                                      match luop with
+  | ELuop(op, cond, _)          ->  let c = codegen_cond info cond in
+                                      match op with
                                       | LuopNot -> Llvm.build_neg c "negtemp" info.builder
-  | EComp(comp, expr1, expr2, _)  ->  let e1 = codegen_expr info expr1
-                                      and e2 = codegen_expr info expr2 in
-                                      match comp with
+  | EComp(op, expr1, expr2, _)  ->  let e1 = codegen_expr info expr1
+                                    and e2 = codegen_expr info expr2 in
+                                      match op with
                                       | CompEq   -> Llvm.build_icmp Llvm.Icmp.Eq  e1 e2 "if_cond" info.builder
                                       | CompNeq  -> Llvm.build_icmp Llvm.Icmp.Ne  e1 e2 "if_cond" info.builder
                                       | CompGr   -> Llvm.build_icmp Llvm.Icmp.Sgt e1 e2 "if_cond" info.builder
@@ -92,7 +76,7 @@ let rec codegen_cond info cond =
                                       | CompGrEq -> Llvm.build_icmp Llvm.Icmp.Sge e1 e2 "if_cond" info.builder
                                       | CompLsEq -> Llvm.build_icmp Llvm.Icmp.Sle e1 e2 "if_cond" info.builder
 
-let codegen_if info cond stmt =
+and codegen_if info cond stmt =
   let c       = codegen_cond info cond in
   let bb      = Llvm.insertion_block info.builder in
   let f       = Llvm.block_parent bb in
@@ -104,7 +88,7 @@ let codegen_if info cond stmt =
   ignore (Llvm.build_br after_bb info.builder);
   Llvm.position_at_end else_bb info.builder;
 
-let codegen_ifelse info cond stmt1 stmt2 =
+and codegen_ifelse info cond stmt1 stmt2 =
   let c       = codegen_cond info cond in
   let bb      = Llvm.insertion_block info.builder in
   let f       = Llvm.block_parent bb in
@@ -120,7 +104,7 @@ let codegen_ifelse info cond stmt1 stmt2 =
   ignore (Llvm.build_br after_bb info.builder);
   Llvm.position_at_end after_bb info.builder;
 
-let codegen_while info cond stmt =
+and codegen_while info cond stmt =
   let bb      = Llvm.insertion_block info.builder in
   let f       = Llvm.block_parent bb in
   let cond_bb = Llvm.append_block info.context "cond" f in
@@ -135,7 +119,7 @@ let codegen_while info cond stmt =
   ignore (Llvm.build_br cond_bb info.builder);
   Llvm.position_at_end after_bb info.builder;
 
-let codegen_ret info = Llvm.build_ret_void info.builder
-let codegen_retval info expr = Llvm.build_ret (codegen_expr info expr) info.builder
+and codegen_ret info = Llvm.build_ret_void info.builder
+and codegen_retval info expr = Llvm.build_ret (codegen_expr info expr) info.builder
 
 (* define main - compile and dump function *)
